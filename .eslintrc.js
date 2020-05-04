7d10138404c7056b60fe07ac928fe90af254acb2
Josh Hill <josh.hill189@gmail.com>
2020-05-04 13:34:44 +1200

    Remove Config from package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,12 @@ module.exports = {
         browser: true,
         es6: true,
     },
-    extends: ["eslint:recommended", "plugin:react/recommended"],
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+    ],
     globals: {
         Atomics: "readonly",
         SharedArrayBuffer: "readonly",
