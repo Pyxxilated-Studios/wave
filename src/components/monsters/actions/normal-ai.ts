19b9649531c1f19fc945fa7b5083e92093b29bf6
Josh Hill <josh.hill189@gmail.com>
2020-05-29 20:34:04 +1200

    Sort of fix Monster Movement

--- src/components/monsters/actions/normal-ai.ts
+++ src/components/monsters/actions/normal-ai.ts
@@ -6,114 +6,160 @@ import { Direction, Monster, Point } from "../../../types";
 import { attackPlayer } from "./attack-player";
 import { playerInRange, monsterCanMoveTo, isInFieldOfView } from "./move-monster";
 
-const Directions = [Direction.North, Direction.East, Direction.South, Direction.West];
-const getRandomDirection = (): Direction => Directions[Math.random() * Directions.length];
-
-// recursive function for moving the monster to the next available tile
-// will try to go towards the player if possible
-export const move = (
-    direction: Direction,
-    position: Point,
-    currentMap: string,
-    id: string,
-    count: number,
-): RootThunk => async (dispatch, getState): Promise<void> => {
-    count++;
-    // dont allow for infinite loops when monster can't move
-    if (count >= 5) return;
-
-    const { player, world, monsters } = getState();
-
-    const nextPosition = { ...position };
-
-    switch (direction) {
-        case Direction.North: {
-            --nextPosition.y;
-
-            if (
-                !monsterCanMoveTo(
-                    nextPosition,
-                    id,
-                    currentMap,
-                    player.position,
-                    monsters.entities[currentMap],
-                    world.maps[world.floorNumber - 1].tiles,
-                )
-            ) {
-                return dispatch(move(Direction.West, position, currentMap, id, count));
-            } else {
-                position = nextPosition;
-            }
-
-            break;
-        }
-
-        case Direction.South: {
-            ++nextPosition.y;
-
-            if (
-                !monsterCanMoveTo(
-                    nextPosition,
-                    id,
-                    currentMap,
-                    player.position,
-                    monsters.entities[currentMap],
-                    world.maps[world.floorNumber - 1].tiles,
-                )
-            ) {
-                return dispatch(move(Direction.East, position, currentMap, id, count));
-            } else {
-                position = nextPosition;
-            }
-
-            break;
-        }
-
-        case Direction.West: {
-            --nextPosition.x;
-
-            if (
-                !monsterCanMoveTo(
-                    nextPosition,
-                    id,
-                    currentMap,
-                    player.position,
-                    monsters.entities[currentMap],
-                    world.maps[world.floorNumber - 1].tiles,
-                )
-            ) {
-                return dispatch(move(Direction.South, position, currentMap, id, count));
-            } else {
-                position = nextPosition;
-            }
-
-            break;
-        }
-
-        case Direction.East: {
-            ++nextPosition.x;
-
-            if (
-                !monsterCanMoveTo(
-                    nextPosition,
-                    id,
-                    currentMap,
-                    player.position,
-                    monsters.entities[currentMap],
-                    world.maps[world.floorNumber - 1].tiles,
-                )
-            ) {
-                return dispatch(move(Direction.North, position, currentMap, id, count));
-            } else {
-                position = nextPosition;
-            }
-
-            break;
-        }
-
-        default:
-            break;
+/**
+ * Shuffles array in place using the Fisher-Yates Shuffle algorithm
+ *
+ * @param a items An array containing the items.
+ */
+const shuffle = <T>(array: T[]): T[] => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const x = array[i];
+        array[i] = array[j];
+        array[j] = x;
     }
+
+    return array;
+};
+
+/**
+ * Generate a list of ranked ordered positions to move in, based on where the player is in relation
+ * to the monsters location.
+ *
+ * @param location The monsters location
+ * @param playerPosition The players position
+ */
+const makeRankedPositions = (location: Point, playerPosition: Point): Point[] => {
+    const difference = { x: location.x - playerPosition.x, y: location.y - playerPosition.y };
+
+    const rankedPositions = [];
+
+    if (difference.y < 0) {
+        rankedPositions.push({ x: location.x, y: location.y + 1 });
+
+        if (difference.x <= 0) {
+            rankedPositions.push({ x: location.x + 1, y: location.y });
+
+            if (Math.abs(difference.x) < Math.abs(difference.y)) {
+                rankedPositions.push({ x: location.x - 1, y: location.y });
+                rankedPositions.push({ x: location.x, y: location.y - 1 });
+            } else {
+                rankedPositions.push({ x: location.x, y: location.y - 1 });
+                rankedPositions.push({ x: location.x - 1, y: location.y });
+            }
+        } else {
+            rankedPositions.push({ x: location.x - 1, y: location.y });
+
+            if (Math.abs(difference.x) < Math.abs(difference.y)) {
+                rankedPositions.push({ x: location.x + 1, y: location.y });
+                rankedPositions.push({ x: location.x, y: location.y - 1 });
+            } else {
+                rankedPositions.push({ x: location.x, y: location.y - 1 });
+                rankedPositions.push({ x: location.x + 1, y: location.y });
+            }
+        }
+    } else if (difference.y > 0) {
+        rankedPositions.push({ x: location.x, y: location.y - 1 });
+
+        if (difference.x <= 0) {
+            rankedPositions.push({ x: location.x + 1, y: location.y });
+
+            if (Math.abs(difference.x) < Math.abs(difference.y)) {
+                rankedPositions.push({ x: location.x + 1, y: location.y });
+                rankedPositions.push({ x: location.x, y: location.y + 1 });
+            } else {
+                rankedPositions.push({ x: location.x, y: location.y + 1 });
+                rankedPositions.push({ x: location.x + 1, y: location.y });
+            }
+        } else {
+            rankedPositions.push({ x: location.x - 1, y: location.y });
+
+            if (Math.abs(difference.x) < Math.abs(difference.y)) {
+                rankedPositions.push({ x: location.x + 1, y: location.y });
+                rankedPositions.push({ x: location.x, y: location.y + 1 });
+            } else {
+                rankedPositions.push({ x: location.x, y: location.y + 1 });
+                rankedPositions.push({ x: location.x + 1, y: location.y });
+            }
+        }
+    } else if (difference.x < 0) {
+        rankedPositions.push({ x: location.x + 1, y: location.y });
+
+        if (difference.y <= 0) {
+            rankedPositions.push({ x: location.x, y: location.y + 1 });
+
+            if (Math.abs(difference.x) < Math.abs(difference.y)) {
+                rankedPositions.push({ x: location.x - 1, y: location.y });
+                rankedPositions.push({ x: location.x, y: location.y - 1 });
+            } else {
+                rankedPositions.push({ x: location.x, y: location.y - 1 });
+                rankedPositions.push({ x: location.x - 1, y: location.y });
+            }
+        } else {
+            rankedPositions.push({ x: location.x, y: location.y - 1 });
+
+            if (Math.abs(difference.x) < Math.abs(difference.y)) {
+                rankedPositions.push({ x: location.x - 1, y: location.y });
+                rankedPositions.push({ x: location.x, y: location.y + 1 });
+            } else {
+                rankedPositions.push({ x: location.x, y: location.y + 1 });
+                rankedPositions.push({ x: location.x - 1, y: location.y });
+            }
+        }
+    } else {
+        rankedPositions.push({ x: location.x - 1, y: location.y });
+
+        if (difference.y <= 0) {
+            rankedPositions.push({ x: location.x, y: location.y + 1 });
+
+            if (Math.abs(difference.x) < Math.abs(difference.y)) {
+                rankedPositions.push({ x: location.x + 1, y: location.y });
+                rankedPositions.push({ x: location.x, y: location.y - 1 });
+            } else {
+                rankedPositions.push({ x: location.x, y: location.y - 1 });
+                rankedPositions.push({ x: location.x + 1, y: location.y });
+            }
+        } else {
+            rankedPositions.push({ x: location.x, y: location.y - 1 });
+
+            if (Math.abs(difference.x) < Math.abs(difference.y)) {
+                rankedPositions.push({ x: location.x + 1, y: location.y });
+                rankedPositions.push({ x: location.x, y: location.y + 1 });
+            } else {
+                rankedPositions.push({ x: location.x, y: location.y + 1 });
+                rankedPositions.push({ x: location.x + 1, y: location.y });
+            }
+        }
+    }
+
+    return rankedPositions;
+};
+
+/**
+ * Move in a ranked position order -- such that if the player is up and to the left, then the monster will first try to
+ * move up, and then trying to move left if it fails to do so. The other two directions are not important, but are used
+ * when not able to move in the preferred direction.
+ *
+ * @param monster
+ * @param currentMap
+ */
+export const move = (monster: Monster, currentMap: string): RootThunk => async (dispatch, getState): Promise<void> => {
+    const { player, world, monsters } = getState();
+    const { id, location, direction } = monster;
+
+    const rankedPositions = makeRankedPositions(location, player.position);
+
+    const position =
+        rankedPositions.find((position) =>
+            monsterCanMoveTo(
+                position,
+                id,
+                currentMap,
+                player.position,
+                monsters.entities[currentMap],
+                world.maps[world.floorNumber - 1].tiles,
+            ),
+        ) || location;
 
     // recalculate if the monster is in sight
     const inSight = isInFieldOfView(getState().map.sightBox, position);
@@ -126,14 +172,13 @@ export const move = (
         dispatch(hideMonster(id, currentMap));
     }
 
-    const monster = getState().monsters.entities[currentMap][id] as Monster;
     // move the monster
     dispatch(
         monsterMove(
             id,
             currentMap,
             position,
-            direction === Direction.North || direction === Direction.South ? monster.direction : direction,
+            position.x < location.x ? Direction.West : position.x > location.x ? Direction.East : direction,
         ),
     );
 };
@@ -142,60 +187,47 @@ export const moveNormally = (sightBox: Point[], currentMap: string, monster: Mon
     dispatch,
     getState,
 ): Promise<void> => {
-    const { id, location } = monster;
+    const { player, monsters, world } = getState();
+    const { id, location, direction } = monster;
 
     const monsterVisible = isInFieldOfView(sightBox, location);
 
     if (monsterVisible) {
         dispatch(revealMonster(id, currentMap));
 
-        const { player } = getState();
         // check if player is in range
         if (playerInRange(player.position, location)) {
             dispatch(attackPlayer(monster));
         } else {
-            // no player in range, time to move!
-            // get the monsters actual position in pixels
-            // get distance from player on both axis
-            const difference = { x: location.x - player.position.x, y: location.y - player.position.y };
-
-            const greaterY = Math.abs(difference.y) > Math.abs(difference.x);
-            // see if y axis is greater distance from player
-            if (greaterY) {
-                // if the monster is mostly below the player on the y axis
-                if (difference.y > 0) {
-                    // move the monster 'up' relatively
-                    dispatch(move(Direction.North, location, currentMap, id, 0));
-                } else if (difference.y < 0) {
-                    // if the monster is mostly above the player on the y axis
-
-                    // move the monster 'down' relatively
-                    dispatch(move(Direction.South, location, currentMap, id, 0));
-                }
-            } else {
-                // x axis is greater distance from player
-
-                // if the monster is mostly to the right of the player
-                if (difference.x > 0) {
-                    // move the monster 'left' relatively
-                    dispatch(move(Direction.West, location, currentMap, id, 0));
-                } else if (difference.x < 0) {
-                    // if the monster is mostly to the left of the player
-
-                    // move the monster 'right' relatively
-                    dispatch(move(Direction.East, location, currentMap, id, 0));
-                }
-            }
+            // The player isn't in range, but they are within our field of view
+            dispatch(move(monster, currentMap));
         }
     } else {
         // monster is too far away from the player
         dispatch(hideMonster(id, currentMap));
 
-        // give a 25% chance to move the monster when hidden
-        if (Math.round(Math.random() * (4 - 1) + 1) !== 4) {
-            const randomDirection = getRandomDirection();
-            // move the monster in a random direction
-            dispatch(move(randomDirection, location, currentMap, id, 0));
+        // Give monsters a chance to move
+        if (Math.floor(Math.random() * 4) + 1 !== 4) {
+            const position =
+                shuffle(makeRankedPositions(location, player.position)).find((position) =>
+                    monsterCanMoveTo(
+                        position,
+                        id,
+                        currentMap,
+                        player.position,
+                        monsters.entities[currentMap],
+                        world.maps[world.floorNumber - 1].tiles,
+                    ),
+                ) || location;
+
+            dispatch(
+                monsterMove(
+                    id,
+                    currentMap,
+                    position,
+                    position.x < location.x ? Direction.West : position.x > location.x ? Direction.East : direction,
+                ),
+            );
         }
     }
 };
