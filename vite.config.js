9b57105e4475957419bccfd5089048171b227195
Josh Hill <josh@pyxxilated.studio>
2021-12-04 17:15:39 +1300

    fix: Vite dependency

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,26 @@ import react from "@vitejs/plugin-react";
 import wasmPack from "vite-plugin-wasm-pack";
 
 export default defineConfig({
-    plugins: [
-        wasmPack(["./wave"]),
-        react(),
-    ],
+    plugins: [wasmPack(["./wave"]), react()],
     root: "src",
     publicDir: "../public",
     build: {
         outDir: "../dist",
         rollupOptions: {
             output: {
-                entryFileNames: "[name].js"
-            }
-        }
-    }
+                manualChunks: {
+                    'react-redux': ["react", "react-dom", "redux", "redux-thunk", "react-redux"],
+                    fonts: [
+                        "@fortawesome/fontawesome-free",
+                        "@fortawesome/fontawesome-svg-core",
+                        "@fortawesome/free-solid-svg-icons",
+                        "@fortawesome/react-fontawesome",
+                        "typeface-roboto",
+                        "typeface-roboto-mono",
+                        "typeface-montserrat",
+                    ],
+                },
+            },
+        },
+    },
 });
